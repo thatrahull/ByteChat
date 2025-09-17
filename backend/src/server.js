7765c95258787ac3b319a26c6db9f89fea152b96
thatrahull <rahul.kumarmahato92@gmail.com>
2025-09-17 22:27:08 +0530

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,14 +42,7 @@ if(process.env.NODE_ENV === "production") {
   })
 }
 
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
